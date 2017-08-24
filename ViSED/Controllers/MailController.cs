dc6cca0904afa997d0c15400289786cfd4a791ea
Апти Хасухаджиев <apti.has@gmail.com>
2017-08-24 22:16:17 +0300

    Добавлен контроллер для почты

---
+++ ViSED/Controllers/MailController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViSED.Controllers
+{
+    public class MailController : Controller
+    {
+        // GET: Mail
+        public ActionResult IndexEmail()
+        {
+            return View();
+        }
+
+        public ActionResult NewEmail()
+        {
+            return View();
+        }
+
+        public ActionResult EmailList()
+        {
+            return View();
+        }
+    }
+}
