68ddf4d5fc0f42f4c9ec46a0a2c2f5ae856e1479
Апти Хасухаджиев <apti.has@gmail.com>
2017-09-04 23:50:41 +0300

    Доработаны мелкие ошибки!

---
+++ ViSED/ProgramLogic/UniverseClassViSed.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViSED.ProgramLogic
+{
+    public static class UniverseClassViSed
+    {
+        public static string Words(string _str, int _num)
+        {
+            if(_str.Split(' ').Length > _num)
+            {
+                string str=null;
+                for(int i = 0; i < _num; i++)
+                {
+                    str += _str.Split(' ')[i]+" ";
+                }
+                return str.Trim()+" ...";
+            }
+            else
+            {
+                return _str;
+            }
+        }
+    }
+}
